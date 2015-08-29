2878ba0d138fabc740d9e430bd739d8eb19efa50
John Brunton <jbrunton@zipcar.com>
2015-08-29 11:42:39 +0100

    list timelines in recyclerview

--- app/src/main/java/com/jbrunton/pockettimeline/app/TimelinesFragment.java
+++ app/src/main/java/com/jbrunton/pockettimeline/app/TimelinesFragment.java
@@ -3,9 +3,12 @@ package com.jbrunton.pockettimeline.app;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jbrunton.pockettimeline.R;
@@ -13,6 +16,9 @@ import com.jbrunton.pockettimeline.api.DaggerProvidersComponent;
 import com.jbrunton.pockettimeline.api.ProvidersComponent;
 import com.jbrunton.pockettimeline.models.Timeline;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import rx.android.schedulers.AndroidSchedulers;
@@ -20,10 +26,23 @@ import rx.schedulers.Schedulers;
 
 public class TimelinesFragment extends Fragment {
     final ProvidersComponent providers = DaggerProvidersComponent.create();
+    private TimelinesListAdapter timelinesAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_timelines, container, false);
+        RecyclerView view = (RecyclerView) inflater.inflate(R.layout.fragment_timelines, container, false);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        view.setHasFixedSize(true);
+
+        view.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        timelinesAdapter = new TimelinesListAdapter();
+        view.setAdapter(timelinesAdapter);
+
+
+        return view;
     }
 
     @Override
@@ -39,10 +58,8 @@ public class TimelinesFragment extends Fragment {
                 );
     }
 
-
-
     private void onTimelinesAvailable(List<Timeline> timelines) {
-        showMessage("Timelines: " + timelines.size());
+        timelinesAdapter.setData(timelines);
     }
 
     private void onError(Throwable throwable) {
@@ -51,5 +68,41 @@ public class TimelinesFragment extends Fragment {
 
     private void showMessage(String text) {
         Toast.makeText(this.getActivity(), text, Toast.LENGTH_LONG).show();
+    }
+
+    private static class TimelinesListAdapter extends RecyclerView.Adapter<TimelinesListAdapter.ViewHolder> {
+        private List<Timeline> timelines = new ArrayList<>();
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            private TextView textView;
+
+            public ViewHolder(TextView textView) {
+                super(textView);
+                this.textView = textView;
+            }
+        }
+
+        @Override
+        public TimelinesListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+            TextView textView = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+
+            return new ViewHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(TimelinesListAdapter.ViewHolder holder, int position) {
+            holder.textView.setText(timelines.get(position).getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return timelines.size();
+        }
+
+        public void setData(Collection<Timeline> timelines) {
+            this.timelines = new ArrayList<>(timelines);
+            notifyDataSetChanged();
+        }
     }
 }
