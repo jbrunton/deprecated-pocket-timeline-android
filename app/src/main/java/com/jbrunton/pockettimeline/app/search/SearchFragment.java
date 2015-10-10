fbf73de29018a5f58646d62e8ab18cce44260af2
John Brunton <jbrunton@zipcar.com>
2015-10-10 14:03:17 +0100

    #21 persist search query across config change

--- app/src/main/java/com/jbrunton/pockettimeline/app/search/SearchFragment.java
+++ app/src/main/java/com/jbrunton/pockettimeline/app/search/SearchFragment.java
@@ -2,6 +2,7 @@ package com.jbrunton.pockettimeline.app.search;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -24,9 +25,14 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import rx.Observable;
+
 public class SearchFragment extends BaseFragment {
     @Inject EventsProvider eventsProvider;
     private EventsAdapter eventsAdapter;
+    private final String SEARCH_CACHE_KEY = "search";
+    private String query;
+    private SearchView searchView;
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         RecyclerView view = (RecyclerView) inflater.inflate(R.layout.recycler_view, container, false);
@@ -42,28 +48,51 @@ public class SearchFragment extends BaseFragment {
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         applicationComponent().inject(this);
+
+        if (savedInstanceState != null) {
+            query = savedInstanceState.getString("query");
+        }
     }
 
     @Override public void onResume() {
         super.onResume();
         setTitle("Search");
         setHasOptionsMenu(true);
+
+        subscribeTo(fetch(SEARCH_CACHE_KEY),
+                this::searchResultsAvailable);
+    }
+
+    @Override public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("query", query);
     }
 
     @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_search, menu);
 
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) searchMenuItem.getActionView();
+        if (query != null && !query.isEmpty()) {
+            searchMenuItem.expandActionView();
+            searchView.setQuery(query, false);
+        }
         searchView.setOnQueryTextListener(onQueryTextListener);
     }
 
     private void searchFor(String query) {
+        this.query = query;
         if (query.isEmpty()) {
             eventsAdapter.setDataSource(Collections.<Event>emptyList());
         } else {
-            subscribeTo(eventsProvider.searchEvents(query), this::searchResultsAvailable);
+            subscribeTo(cache(SEARCH_CACHE_KEY, () -> doSearch(query)),
+                    this::searchResultsAvailable);
         }
+    }
+
+    private Observable<List<Event>> doSearch(String query) {
+        return eventsProvider.searchEvents(query);
     }
 
     private void searchResultsAvailable(List<Event> events) {
