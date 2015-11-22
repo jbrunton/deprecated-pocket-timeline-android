22a07944c11dc16c22fc7303d23836c345204bfc
John Brunton <jbrunton@zipcar.com>
2015-11-22 12:39:17 +0000

    #34 remove TimelineFragment, move logic to activity

--- app/src/main/java/com/jbrunton/pockettimeline/app/ApplicationComponent.java
+++ app/src/main/java/com/jbrunton/pockettimeline/app/ApplicationComponent.java
@@ -4,18 +4,18 @@ import com.jbrunton.pockettimeline.api.providers.ProvidersModule;
 import com.jbrunton.pockettimeline.api.service.RestServiceModule;
 import com.jbrunton.pockettimeline.app.quiz.QuizFragment;
 import com.jbrunton.pockettimeline.app.search.SearchFragment;
-import com.jbrunton.pockettimeline.app.timelines.TimelineFragment;
+import com.jbrunton.pockettimeline.app.timelines.TimelineActivity;
 import com.jbrunton.pockettimeline.app.timelines.TimelinesFragment;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
-import rx.Scheduler;
 
 @Singleton
 @Component(modules = {RestServiceModule.class, ProvidersModule.class})
 public interface ApplicationComponent {
-    void inject(TimelineFragment fragment);
+    void inject(TimelineActivity activity);
+
     void inject(TimelinesFragment fragment);
     void inject(QuizFragment fragment);
     void inject(SearchFragment fragment);
--- app/src/main/java/com/jbrunton/pockettimeline/app/timelines/TimelineActivity.java
+++ app/src/main/java/com/jbrunton/pockettimeline/app/timelines/TimelineActivity.java
@@ -3,30 +3,88 @@ package com.jbrunton.pockettimeline.app.timelines;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.jbrunton.pockettimeline.R;
+import com.jbrunton.pockettimeline.api.providers.EventsProvider;
+import com.jbrunton.pockettimeline.api.providers.TimelinesProvider;
 import com.jbrunton.pockettimeline.app.shared.BaseActivity;
+import com.jbrunton.pockettimeline.models.Timeline;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+
+import static rx.Observable.zip;
 
 public class TimelineActivity extends BaseActivity {
     private static final String ARG_TIMELINE_ID = "timelineId";
+    private static final String TIMELINE_CACHE_KEY = "timeline";
+
+    @Inject TimelinesProvider timelinesProvider;
+    @Inject EventsProvider eventsProvider;
+    private EventsAdapter eventsAdapter;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment_holder);
+        setContentView(R.layout.activity_timeline);
 
-        if (savedInstanceState == null) {
-            String timelineId = getIntent().getStringExtra(ARG_TIMELINE_ID);
-            TimelineFragment fragment = TimelineFragment.newInstance(timelineId);
+        FloatingActionButton addEvent = (FloatingActionButton) findViewById(R.id.add_event);
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                showMessage("Hey");
+            }
+        });
 
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_holder, fragment)
-                    .commit();
-        }
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        eventsAdapter = new EventsAdapter();
+        recyclerView.setAdapter(eventsAdapter);
+
+        applicationComponent().inject(this);
     }
 
     public static void start(Context context, String timelineId) {
         Intent intent = new Intent(context, TimelineActivity.class);
         intent.putExtra(ARG_TIMELINE_ID, timelineId);
         context.startActivity(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        setTitle("Timeline");
+        setHomeAsUp(true);
+
+        cache(TIMELINE_CACHE_KEY, this::getTimeline)
+                .compose(applySchedulers())
+                .subscribe(this::onTimelineAvailable, this::defaultErrorHandler);
+    }
+
+    @Override protected String ownerId() {
+        return getTimelineId();
+    }
+
+    private Observable<Timeline> getTimeline() {
+        String timelineId = getTimelineId();
+        return zip(
+                timelinesProvider.getTimeline(timelineId),
+                eventsProvider.getEvents(timelineId),
+                Timeline::withEvents
+        );
+    }
+
+    private void onTimelineAvailable(Timeline timeline) {
+        setTitle(timeline.getTitle());
+        eventsAdapter.setDataSource(timeline.getEvents());
+    }
+
+    private String getTimelineId() {
+        return getIntent().getStringExtra(ARG_TIMELINE_ID);
     }
 }
