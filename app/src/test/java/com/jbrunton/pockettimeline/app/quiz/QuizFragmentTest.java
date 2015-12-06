3d6fbd319a069b82f41325bdb4861a263b25295f
John Brunton <jbrunton@zipcar.com>
2015-12-06 16:15:28 +0000

    #40 remove redundant tests from QuizFragmentTest

--- app/src/test/java/com/jbrunton/pockettimeline/app/quiz/QuizFragmentTest.java
+++ app/src/test/java/com/jbrunton/pockettimeline/app/quiz/QuizFragmentTest.java
@@ -3,10 +3,8 @@ package com.jbrunton.pockettimeline.app.quiz;
 import android.app.Dialog;
 
 import com.jbrunton.pockettimeline.R;
-import com.jbrunton.pockettimeline.api.providers.EventsProvider;
 import com.jbrunton.pockettimeline.api.service.RestServiceModule;
 import com.jbrunton.pockettimeline.app.ApplicationComponent;
-import com.jbrunton.pockettimeline.fixtures.DeterministicRandomHelperModule;
 import com.jbrunton.pockettimeline.fixtures.FragmentTestSuite;
 import com.jbrunton.pockettimeline.fixtures.TestProvidersModule;
 import com.jbrunton.pockettimeline.models.Event;
@@ -18,52 +16,51 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 
-import java.util.Arrays;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import dagger.Component;
-import rx.Observable;
+import dagger.Module;
+import dagger.Provides;
 
 import static com.jbrunton.pockettimeline.fixtures.AlertDialogFixtures.alertDialogButton;
 import static com.jbrunton.pockettimeline.fixtures.AlertDialogFixtures.alertDialogMessage;
 import static com.jbrunton.pockettimeline.fixtures.AlertDialogFixtures.alertDialogShowing;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class QuizFragmentTest extends FragmentTestSuite<QuizFragment, QuizFragmentTest.TestApplicationComponent> {
-    @Inject EventsProvider eventsProvider;
-    final Event EVENT_ONE = new Event("1", new LocalDate(2014, DateTimeConstants.JUNE, 3), "Event One", null);
-    final Event EVENT_TWO = new Event("2", new LocalDate(2015, DateTimeConstants.JUNE, 4), "Event Two", null);
+    @Inject QuizPresenter presenter;
+
+    final Event EVENT = new Event("1", new LocalDate(2014, DateTimeConstants.JUNE, 3), "Event One", null);
 
     @Before public void setUp() {
         configureTestSuite(new QuizFragment());
 
         component().inject(this);
-        stubProviderToReturn(EVENT_ONE, EVENT_TWO);
 
         controller().start().resume();
     }
 
     @Test public void shouldDisplayEventDetails() {
-        assertThat(eventTitle()).isEqualTo(EVENT_TWO.getTitle());
+        fragment().displayEvent(EVENT);
+        assertThat(eventTitle()).isEqualTo(EVENT.getTitle());
     }
 
-    @Test public void shouldRespondToCorrectAnswer() {
-        submitAnswer(EVENT_TWO.getDate());
+    @Test public void shouldShowCorrectDialog() {
+        fragment().showCorrectDialog();
         assertThat(alertDialogMessage()).isEqualTo(getString(R.string.correct_answer));
     }
 
-    @Test public void shouldRespondToIncorrectAnswer() {
-        submitAnswer(EVENT_ONE.getDate());
+    @Test public void shouldShowIncorrectDialog() {
+        fragment().showIncorrectDialog("2015");
         assertThat(alertDialogMessage()).isEqualTo(getString(R.string.incorrect_answer, "2015"));
     }
 
     @Test public void shouldAllowResponseDialogToBeDismissed() {
-        submitAnswer(EVENT_TWO.getDate());
+        fragment().showCorrectDialog();
         assertThat(alertDialogShowing()).isTrue();
 
         alertDialogButton(Dialog.BUTTON_POSITIVE).performClick();
@@ -71,27 +68,16 @@ public class QuizFragmentTest extends FragmentTestSuite<QuizFragment, QuizFragme
         assertThat(alertDialogShowing()).isFalse();
     }
 
-    @Test public void shouldAskTheNextQuestionAfterAnswer() {
-        submitAnswer(EVENT_TWO.getDate());
+    @Test public void shouldAskNextQuestionAfterDialogDismissed() {
+        shouldShowCorrectDialog();
         alertDialogButton(Dialog.BUTTON_POSITIVE).performClick();
-        assertThat(eventTitle()).isEqualTo(EVENT_ONE.getTitle());
+        verify(presenter).nextQuestion();
     }
 
-    @Test public void shouldClearAnswerFieldAfterDialogDismissed() {
+    @Test public void shouldClearAnswerFieldWhenDisplayingEvent() {
         submitAnswer("some answer");
-        assertThat(answer()).isNotEmpty();
-
-        alertDialogButton(Dialog.BUTTON_POSITIVE).performClick();
-
+        fragment().displayEvent(EVENT);
         assertThat(answer()).isEmpty();
-    }
-
-    private void stubProviderToReturn(Event... events) {
-        when(eventsProvider.getEvents()).thenReturn(Observable.just(Arrays.asList(events)));
-    }
-
-    private void submitAnswer(LocalDate date) {
-        submitAnswer(Integer.toString(date.getYear()));
     }
 
     private void submitAnswer(String answer) {
@@ -107,14 +93,19 @@ public class QuizFragmentTest extends FragmentTestSuite<QuizFragment, QuizFragme
         return textView(R.id.answer).getText().toString();
     }
 
-    @Singleton @Component(modules = {RestServiceModule.class, TestProvidersModule.class, DeterministicRandomHelperModule.class})
+    @Singleton @Component(modules = {RestServiceModule.class, TestProvidersModule.class, TestPresentersModule.class})
     public static interface TestApplicationComponent extends ApplicationComponent {
         void inject(QuizFragmentTest test);
     }
 
     @Override protected TestApplicationComponent createComponent() {
-        return DaggerQuizFragmentTest_TestApplicationComponent.builder()
-                .deterministicRandomHelperModule(new DeterministicRandomHelperModule(1, 0))
-                .build();
+        return DaggerQuizFragmentTest_TestApplicationComponent.create();
+    }
+
+    @Module
+    public static class TestPresentersModule {
+        @Provides @Singleton public QuizPresenter provideQuizPresenter() {
+            return mock(QuizPresenter.class);
+        }
     }
 }
