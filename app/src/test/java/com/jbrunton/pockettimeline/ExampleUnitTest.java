eb788a0200db786de7184506dbb53133e2ce947b
John Brunton <jbrunton@zipcar.com>
2016-05-22 19:12:33 +0100

    add critical sonar issue

--- app/src/test/java/com/jbrunton/pockettimeline/ExampleUnitTest.java
+++ app/src/test/java/com/jbrunton/pockettimeline/ExampleUnitTest.java
@@ -12,6 +12,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(new BigDecimal(4), new BigDecimal(2 + 2));
+        assertEquals(new BigDecimal(4.0), new BigDecimal(2 + 2));
     }
 }
