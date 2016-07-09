bd073ef70ec06983d764bdc91829df91e38d940f
John Brunton <jbrunton@zipcar.com>
2016-07-09 16:43:55 +0100

    use material drawer lib

--- app/src/main/java/com/jbrunton/pockettimeline/app/HomeActivity.java
+++ app/src/main/java/com/jbrunton/pockettimeline/app/HomeActivity.java
@@ -1,26 +1,25 @@
 package com.jbrunton.pockettimeline.app;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.util.SparseArray;
-import android.view.MenuItem;
+import android.view.View;
 
 import com.jbrunton.pockettimeline.R;
 import com.jbrunton.pockettimeline.app.quiz.QuizFragment;
 import com.jbrunton.pockettimeline.app.search.SearchFragment;
 import com.jbrunton.pockettimeline.app.shared.BaseActivity;
 import com.jbrunton.pockettimeline.app.timelines.TimelinesFragment;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class HomeActivity extends BaseActivity {
 
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle drawerToggle;
-    
+    private Drawer drawer;
+
     private final SparseArray<Fragment> DRAWER_OPTIONS = new SparseArray<Fragment>() {{
         put(R.id.nav_timelines, new TimelinesFragment());
         put(R.id.nav_quiz, new QuizFragment());
@@ -31,11 +30,10 @@ public class HomeActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        configureActionBar();
-        configureDrawer();
+        drawer = buildDrawer(savedInstanceState);
 
         if (savedInstanceState == null) {
-            selectDrawerOption(R.id.nav_timelines);
+            drawer.setSelection(R.id.nav_timelines);
         }
     }
 
@@ -43,45 +41,30 @@ public class HomeActivity extends BaseActivity {
 
     }
 
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void configureActionBar() {
+    private Drawer buildDrawer(Bundle savedInstanceState) {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer);
-        setHomeAsUp(true);
+        DrawerBuilder builder = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        new PrimaryDrawerItem()
+                                .withIdentifier(R.id.nav_timelines)
+                                .withName("Timelines"),
+                        new PrimaryDrawerItem()
+                                .withIdentifier(R.id.nav_quiz)
+                                .withName("Quiz"),
+                        new PrimaryDrawerItem()
+                                .withIdentifier(R.id.nav_search)
+                                .withName("Search")
+                )
+                .withOnDrawerItemClickListener(this::onDrawerItemSelected)
+                .withSavedInstance(savedInstanceState);
+        return builder.build();
     }
 
-    private void configureDrawer() {
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        NavigationView view = (NavigationView) findViewById(R.id.nav_view);
-        view.setNavigationItemSelectedListener(this::onDrawerItemSelected);
-
-        drawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                R.string.open_drawer,
-                R.string.close_drawer);
-
-        drawerLayout.setDrawerListener(drawerToggle);
-    }
-
-    private boolean onDrawerItemSelected(MenuItem menuItem) {
-        menuItem.setChecked(true);
-        drawerLayout.closeDrawers();
-        selectDrawerOption(menuItem.getItemId());
+    private boolean onDrawerItemSelected(View view, int position, IDrawerItem drawerItem) {
+        selectDrawerOption((int) drawerItem.getIdentifier());
+        drawer.closeDrawer();
         return true;
     }
 
